18fc445b961cd5f1f76fe63c1b018a7f20380763
Mukato11 <maia.kakabadze@teachers.gov.ge>
2025-09-01 14:17:44 +0400

    add files

---
+++ variables.js
@@ -0,0 +1,45 @@
+//task-1
+console.log("task_1");
+let age =   100;
+if (age<50) {console.log("asaki naklebia 50-ze")}
+else if (age>20) {console.log("asaki metia 20-ze")}
+else {console.log("error")}
+console.log("--------------------------------------");
+
+
+
+//task-2
+console.log("task_2");
+let myname='Mariam';
+let secondname;
+if (secondname=="mariam") {console.log("true")}
+else {console.log("false")}
+console.log("--------------------------------------");
+
+
+
+/*
+let myname='Mariam';
+let name_rezalt= 
+ myname=='Mariam' ? "true" : "false";
+console.log(name_rezalt);
+*/
+
+
+
+ 
+//task-3
+console.log("task_3");
+let name1="Mariam";
+switch (name1)
+{
+    case "Mariam":
+    console.log("true");
+    break;
+    default:
+        console.log("false");
+}
+console.log("--------------------------------------");
+
+
+
